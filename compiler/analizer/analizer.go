666793bbea93711c698091b8317bc65d0ada1885
Vova <vboretskyi@gmail.com>
2018-07-15 00:45:36 +0300

    Update analizer, add more commands

--- compiler/analizer/analizer.go
+++ compiler/analizer/analizer.go
@@ -9,12 +9,16 @@ import (
 
 // rule types
 const (
-	RuleTypeClass         = "class"
-	RuleTypeKeyword       = "keyword"
-	RuleTypeIdentifier    = "identifier"
-	RuleTypeSymbol        = "symbol"
-	RuleTypeClassVarDec   = "classVarDec"
-	RuleTypeSubroutineDec = "subroutineDec"
+	RuleTypeClass          = "class"
+	RuleTypeKeyword        = "keyword"
+	RuleTypeIdentifier     = "identifier"
+	RuleTypeSymbol         = "symbol"
+	RuleTypeClassVarDec    = "classVarDec"
+	RuleTypeSubroutineDec  = "subroutineDec"
+	RuleTypeParameterList  = "parameterList"
+	RuleTypeSubroutineBody = "subroutineBody"
+	RuleTypeVarDec         = "varDec"
+	RuleTypeStatements     = "statements"
 )
 
 // Analizer type
@@ -23,7 +27,7 @@ type Analizer struct {
 	tl *[]tokenizer.Token // token list
 }
 
-// New constructs new Analizer
+// New returns new Analizer
 func New(tl *[]tokenizer.Token) *Analizer {
 	return &Analizer{-1, tl}
 }
@@ -41,17 +45,17 @@ func (a *Analizer) CompileClass() *pt.ParseTree {
 	leaf.AddChildren(pt.New(RuleTypeSymbol, a.eat(vld.Identity("{"))))
 	// classVarDec*
 	for {
-		if !vld.OneOf("static", "field")(a.nextToken().S) {
+		if !vld.OneOf("static", "field")(a.getNextToken().S) {
 			break // no more var decs
 		}
-		addIfHasChildren(leaf, a.CompileClassVarDec())
+		addIfHasChildren(leaf, a.compileClassVarDec())
 	}
 	// subroutineDec*
 	for {
-		if !vld.OneOf("constructor", "function", "method")(a.nextToken().S) {
+		if !vld.OneOf("constructor", "function", "method")(a.getNextToken().S) {
 			break // no more subroutines
 		}
-		addIfHasChildren(leaf, a.CompileClassSubroutineDec())
+		addIfHasChildren(leaf, a.compileClassSubroutineDec())
 	}
 	// '}'
 	leaf.AddChildren(pt.New(RuleTypeSymbol, a.eat(vld.Identity("}"))))
@@ -59,8 +63,7 @@ func (a *Analizer) CompileClass() *pt.ParseTree {
 	return leaf
 }
 
-// CompileClassVarDec ...
-func (a *Analizer) CompileClassVarDec() *pt.ParseTree {
+func (a *Analizer) compileClassVarDec() *pt.ParseTree {
 	// Grammar: ('static' | 'field') type varName (',' varName)* ';'
 	leaf := pt.New(RuleTypeClassVarDec, "")
 
@@ -75,7 +78,7 @@ func (a *Analizer) CompileClassVarDec() *pt.ParseTree {
 	for {
 		// varName
 		leaf.AddChildren(pt.New(RuleTypeKeyword, a.eat(vld.IsIdentifier)))
-		if !vld.Identity(",")(a.nextToken().S) {
+		if !vld.Identity(",")(a.getNextToken().S) {
 			break // no more identifiers
 		}
 		// ','
@@ -87,20 +90,126 @@ func (a *Analizer) CompileClassVarDec() *pt.ParseTree {
 	return leaf
 }
 
-// CompileClassSubroutineDec ... TODO
-func (a *Analizer) CompileClassSubroutineDec() *pt.ParseTree {
+func (a *Analizer) compileClassSubroutineDec() *pt.ParseTree {
+	// Grammar: ("constructor" | "function" | "method") ('void' | type)
+	// subroutineName '(' parameterList ')' subroutineBody
 	leaf := pt.New(RuleTypeSubroutineDec, "")
+
+	// ("constructor" | "function" | "method")
+	leaf.AddChildren(pt.New(RuleTypeKeyword, a.eat(vld.OneOf("constructor", "function", "method"))))
+	// ('void' | type)
+	leaf.AddChildren(pt.New(RuleTypeKeyword, a.eat(
+		vld.OneOf("int", "char", "boolean", "void"),
+		vld.IsIdentifier,
+	)))
+	// subroutineName
+	leaf.AddChildren(pt.New(RuleTypeIdentifier, a.eat(vld.IsIdentifier)))
+	// '('
+	leaf.AddChildren(pt.New(RuleTypeIdentifier, a.eat(vld.Identity("("))))
+	// parameterList
+	leaf.AddChildren(a.compileParameterList())
+	// ')'
+	leaf.AddChildren(pt.New(RuleTypeIdentifier, a.eat(vld.Identity(")"))))
+	// subroutineBody
+	leaf.AddChildren(a.compileSubroutineBody())
+
 	return leaf
 }
 
-// get current token value
-func (a *Analizer) currentToken() tokenizer.Token {
+func (a *Analizer) compileParameterList() *pt.ParseTree {
+	// Grammar: ((type varName)(',' type varName)*)?
+	leaf := pt.New(RuleTypeParameterList, "")
+
+	// no parameters
+	if vld.OneOf("int", "char", "boolean")(a.getNextToken().S) {
+		for {
+			// type
+			leaf.AddChildren(pt.New(RuleTypeKeyword, a.eat(
+				vld.OneOf("int", "char", "boolean"),
+				vld.IsIdentifier,
+			)))
+			// varName
+			leaf.AddChildren(pt.New(RuleTypeIdentifier, a.eat(vld.IsIdentifier)))
+			if !vld.Identity(",")(a.getNextToken().S) {
+				break // no more parameters
+			}
+			// ','
+			leaf.AddChildren(pt.New(RuleTypeSymbol, a.eat(vld.Identity(","))))
+		}
+	}
+
+	return leaf
+}
+
+func (a *Analizer) compileSubroutineBody() *pt.ParseTree {
+	// Grammar: '{' varDec* statements '}'
+	leaf := pt.New(RuleTypeSubroutineBody, "")
+
+	// '{'
+	leaf.AddChildren(pt.New(RuleTypeSymbol, a.eat(vld.Identity("{"))))
+	// varDec*
+	for {
+		if !vld.Identity("var")(a.getNextToken().S) {
+			break // no more var decs
+		}
+		leaf.AddChildren(a.compileVarDec())
+	}
+	// statements
+	for {
+		if !vld.OneOf("let", "if", "while", "do", "return")(a.getNextToken().S) {
+			break // no more var decs
+		}
+		leaf.AddChildren(a.compileStatements())
+	}
+	// '}'
+	leaf.AddChildren(pt.New(RuleTypeSymbol, a.eat(vld.Identity("}"))))
+
+	return leaf
+}
+
+// TODO
+func (a *Analizer) compileStatements() *pt.ParseTree {
+	// Grammar: statement*
+	leaf := pt.New(RuleTypeStatements, "")
+
+	leaf.AddChildren(pt.New(RuleTypeKeyword, a.eat(vld.Identity("let"))))
+
+	return leaf
+}
+
+func (a *Analizer) compileVarDec() *pt.ParseTree {
+	// Grammar: 'var' type varName (',' varName)* ';'
+	leaf := pt.New(RuleTypeVarDec, "")
+
+	// 'var'
+	leaf.AddChildren(pt.New(RuleTypeKeyword, a.eat(vld.Identity("var"))))
+	// type
+	leaf.AddChildren(pt.New(RuleTypeKeyword, a.eat(
+		vld.OneOf("int", "char", "boolean"),
+		vld.IsIdentifier,
+	)))
+	// varName (',' varName)*
+	for {
+		// varName
+		leaf.AddChildren(pt.New(RuleTypeIdentifier, a.eat(vld.IsIdentifier)))
+		if !vld.Identity(",")(a.getNextToken().S) {
+			break // no more var identifiers
+		}
+		// ','
+		leaf.AddChildren(pt.New(RuleTypeSymbol, a.eat(vld.Identity(","))))
+	}
+	// ';'
+	leaf.AddChildren(pt.New(RuleTypeKeyword, a.eat(vld.Identity(";"))))
+
+	return leaf
+}
+
+func (a *Analizer) getCurrentToken() tokenizer.Token {
 	t := *a.tl
 	return t[a.ti]
 }
 
-// get next token
-func (a *Analizer) nextToken() tokenizer.Token {
+func (a *Analizer) getNextToken() tokenizer.Token {
 	t := *a.tl
 	return t[a.ti+1]
 }
@@ -109,7 +218,7 @@ func (a *Analizer) nextToken() tokenizer.Token {
 // one of provided rules should pass
 func (a *Analizer) eat(rules ...vld.Rule) string {
 	a.ti++
-	s := a.currentToken().S
+	s := a.getCurrentToken().S
 	for _, r := range rules {
 		if r(s) {
 			return s
@@ -146,10 +255,4 @@ func ToXML(pt *pt.ParseTree, indent int) string {
 	}
 
 	return xml
-}
-
-func handleErr(e error) {
-	if e != nil {
-		panic(e)
-	}
 }
